71d4408419bff4251c7c3ba564cf50887ef5073f
Marcus Gotling <marcus@gotling.se>
2024-10-09 15:26:15 +0200

    Styling

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import requests_cache
 import re
 from bs4 import BeautifulSoup
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+main = 'https://www.realitytvrevisited.com/2016/02/hells-kitchen-contestants.html'
 
 year = 2016
 month = 11
@@ -20,30 +22,30 @@ def get_soup(season):
 
 @app.route("/")
 def index():
-    return """
-    <html><body>
-    <h1>Hells Kitchen</h1>
-    <h2>No spoilers</h2>
-    <a href='/6/1'>Season 6</a><br>
-    <a href='/7/1'>Season 7</a><br>
-    <a href='/8/1'>Season 8</a><br>
-    </body></html>"""
+    return render_template('index.html')
+
 
 @app.route('/<int:season>/<int:episode>')
 def show_post(season, episode):
     soup = get_soup(season)
     result = soup.find_all(string=re.compile("eliminated in Hell's Kitchen season {} episode {}[^0-9]".format(season, episode)))
 
-    content = ""
+    context = {
+        'season': season,
+        'episode': episode,
+        'nav': nav_buttons(season, episode),
+        'content': []
+    }
 
     for person in result:
-        content += str(person.parent)
+        context['content'].append(str(person.parent))
 
-    return "<html><body><h1>Eliminated season {} episode {}</h1>{}<br><div style='width: 600px'>{}</div></body></html>".format(season, episode, nav_buttons(season, episode), content)
+    return render_template('episode.html', **context)
 
 
 def nav_buttons(season, episode):
-    return '<a href=/{}/{}>< Previous</a> <a href=/{}/{}>Next ></a>'.format(season, episode-1, season, episode+1)
+
+   
 
 
 if __name__ == '__main__':
